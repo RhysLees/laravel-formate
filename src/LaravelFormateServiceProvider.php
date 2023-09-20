3d4e35a537d7474f0727fb1743abad1f3b246011
Rhys Lees <43909932+RhysLees@users.noreply.github.com>
2023-09-20 12:07:39 +0100

    INIT

--- src/LaravelFormateServiceProvider.php
+++ src/LaravelFormateServiceProvider.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace VendorName\Skeleton;
+namespace RhysLees\LaravelFormate;
 
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use VendorName\Skeleton\Commands\SkeletonCommand;
+use RhysLees\LaravelFormate\Commands\LaravelFormateCommand;
 
-class SkeletonServiceProvider extends PackageServiceProvider
+class LaravelFormateServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
@@ -16,10 +16,7 @@ class SkeletonServiceProvider extends PackageServiceProvider
          * More info: https://github.com/spatie/laravel-package-tools
          */
         $package
-            ->name('skeleton')
-            ->hasConfigFile()
-            ->hasViews()
-            ->hasMigration('create_skeleton_table')
-            ->hasCommand(SkeletonCommand::class);
+            ->name('laravel-formate')
+            ->hasViews();
     }
 }
