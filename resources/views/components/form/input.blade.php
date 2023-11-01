2885ea4da478cb413987a3d39f81537229ea55f1
Rhys Lees <43909932+RhysLees@users.noreply.github.com>
2023-11-01 12:41:52 +0000

    WIP

--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -46,7 +46,7 @@
                 type="{{ $type }}"
                 value="{{ old($name, $value) }}"
 
-                @if($wire) {{ $wire }} @endif
+                @if($wire) wire:model.live="{{ $wire }}" @endif
 
                 class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6
                 @error($name) text-red-900 ring-red-300 placeholder:text-red-300 focus:ring-red-500 @enderror"
