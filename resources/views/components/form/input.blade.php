d956ac269d06fb780c87fdb673ad59cf584980d2
Rhys Lees <43909932+RhysLees@users.noreply.github.com>
2023-11-01 23:09:00 +0000

    WIP

--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -51,7 +51,7 @@
                 class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6
                 @error($name) text-red-900 ring-red-300 placeholder:text-red-300 focus:ring-red-500 @enderror"
 
-                @error($name) aria-invalid="true" aria-describedby="{{ $name . '-error' }} @enderror"
+                @error($name) aria-invalid="true" aria-describedby="{{ $name . '-error' }}" @enderror
                 @if($required) required @endif
                 @if($placeholder) placeholder="{{ $placeholder }}" @endif
                 @if($autofocus) autofocus @endif
