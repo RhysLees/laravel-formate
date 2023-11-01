72b468a9ae793337a0fd79e40051a3e40a6c570b
Rhys Lees <43909932+RhysLees@users.noreply.github.com>
2023-11-01 12:33:29 +0000

    WIP

--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -16,6 +16,7 @@
 	'showLabel' => true,
 	'showBadge' => true,
     'lightLabel' => false,
+    'wire' => null,
 ])
 
 <div {{ $attributes->merge(['class' => '']) }}>
@@ -44,6 +45,8 @@
                 dusk="{{ $name }}"
                 type="{{ $type }}"
                 value="{{ old($name, $value) }}"
+
+                @if($wire) {{ $wire }} @endif
 
                 class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6
                 @error($name) text-red-900 ring-red-300 placeholder:text-red-300 focus:ring-red-500 @enderror"
--- resources/views/components/form/select.blade.php
+++ resources/views/components/form/select.blade.php
@@ -9,6 +9,7 @@
 	'readonly' => false,
 	'showLabel' => true,
 	'showBadge' => true,
+	'wire' => null,
 ])
 
 <div {{ $attributes->merge(['class' => '']) }}>
@@ -31,6 +32,9 @@
                 id="{{ $name }}"
                 name="{{ $name }}"
                 dusk="{{ $name }}"
+
+                @if($wire) {{ $wire }} @endif
+
                 class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6
                 @error($name) text-red-900 ring-red-300 placeholder:text-red-300 focus:ring-red-500 @enderror"
                 @error($name) aria-invalid="true" aria-describedby="{{ $name . '-error' }} @enderror"
